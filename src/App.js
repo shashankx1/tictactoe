67222443aff8da6611d8ec3c03240e34d5b8e9c2
unknown <shashankkumarx1@gmail.com>
2023-02-03 17:16:19 +0530

    finalcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Board from "./components/Board";
+import { calculateWinner } from "./helpers";
+import "./styles/root.scss";
 
-function App() {
+const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isNextx, setNextx] = useState(false);
+  const winner = calculateWinner(board);
+  const MESSAGE = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${isNextx ? "X" : "O"}`;
+
+  console.log(board);
+  const handleSquareClick = (position) => {
+    if (board[position] || winner) {
+      return;
+    }
+    setBoard((prev) => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isNextx ? "x" : "o";
+        }
+        return square;
+      });
+    });
+    setNextx((prev) => !prev);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TIC TAC TOE</h1>
+      <h2>{MESSAGE}</h2>
+      <Board board={board} handleSquareClick={handleSquareClick} />
+      <h6> DEVELOPED BY SHASHANK </h6>
     </div>
   );
-}
+};
 
 export default App;
